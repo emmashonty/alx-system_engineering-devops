dc705c036d24c86f16c77ec51a6e1b2f8c24eb9e
Joseph Emmanuel <102731916+emmashonty@users.noreply.github.com>
2024-01-31 15:37:04 +0100

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
-str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
-str.split(/\w+:(.*), to:(.*), flags:(.*)/)
-puts str
-
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
